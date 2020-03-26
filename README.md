e75f757d1e2a9048aae5b1568d98d7f526ac9dfe
joker7blue <legrandgeorges754@gmail.com>
2020-03-26 19:19:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# background-generator
+A cool background generator
---
+++ script.js
@@ -0,0 +1,42 @@
+let body = document.querySelector("body");
+let result = document.getElementById("result-selected");
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+
+
+
+/* TO UPDATE THE BACKGROUD */
+function setBackground() {
+
+	body.style.background = 'linear-gradient(to right, '+color1.value+', '+color2.value+')';
+	result.textContent = body.style.background;
+}
+
+
+/* INITIALIZATION FUNCTION (TO SET DEFAULT COLOR) */
+function initialize(firstColor, secondColor) {
+
+	color1.value = firstColor;
+	color2.value = secondColor;
+	setBackground();
+}
+
+
+
+
+
+/****************************************  PROCESSING **************************************************/
+initialize('#ff8000', '#008080');
+
+
+color1.addEventListener('input' , (event) => {
+
+	setBackground();
+});
+
+
+
+color2.addEventListener('input' , (event) => {
+
+	setBackground();
+});
